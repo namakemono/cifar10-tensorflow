b83f182fc6b642ed596f2945cef93cd80fa2ea7d
Kei Nakazawa <muo@muo.jp>
2016-05-13 14:53:54 +0900

    Maintain install script
     - `input` directory should exist prior to copying

--- install.sh
+++ install.sh
@@ -2,5 +2,6 @@
 
 curl -O https://www.cs.toronto.edu/~kriz/cifar-10-python.tar.gz \
  && tar xvzfo cifar-10-python.tar.gz \
+ && mkdir -p input \
  && mv cifar-10-batches-py input/ \
  && rm cifar-10-python.tar.gz
