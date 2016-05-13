58d9a4834120299462159d286a67dcd5a1333db4
Kei Nakazawa <muo@muo.jp>
2016-05-13 12:53:43 +0900

    Improve install script a bit

--- install.sh
+++ install.sh
@@ -1,4 +1,6 @@
-wget https://www.cs.toronto.edu/~kriz/cifar-10-python.tar.gz
-tar xvzf cifar-10-python.tar.gz
-mv cifar-10-batches-py input/
-rm cifar-10-python.tar.gz
+#!/bin/bash
+
+curl -O https://www.cs.toronto.edu/~kriz/cifar-10-python.tar.gz \
+ && tar xvzfo cifar-10-python.tar.gz \
+ && mv cifar-10-batches-py input/ \
+ && rm cifar-10-python.tar.gz
